c97933613f94706d4e213bcf44479ce3fa19aa18
Deepesh Kumar <deepesh.kumar.ug21@nsut.ac.in>
2023-10-07 16:36:31 +0530

    User Profile Page Template

---
+++ frontend/pages/profile/[[...index]].tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import HomeNav from '@/components/HomeNav';
+const Profile = () => {
+  return (
+    <div className="">
+      <div className="flex justify-center items-center border-b border-gray-600 mx-2">
+        <HomeNav />
+      </div>
+    </div>
+  )
+}
+
+export default Profile
