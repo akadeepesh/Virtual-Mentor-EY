937dcb3bee27fa523f570aa8a2d5a45b7f2c8a43
Deepesh Kumar <deepesh.kumar.ug21@nsut.ac.in>
2023-10-09 12:36:29 +0530

    Profile template

--- frontend/components/ProfileHero.tsx
+++ frontend/components/ProfileHero.tsx
@@ -66,46 +66,51 @@ const ProfileHero = () => {
       type FormFieldName = "FullName" | "Email" | "ContactNumber" | "Education" | "CareerGoals" | "Interests" | "Skills" | "WorkExperience" | "LearningMethods";
 
       const renderField = (name: FormFieldName, label: string, description: string) => (
-        <FormField
-            control={form.control}
-            name={name}
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>{label}</FormLabel>
-                <FormControl className='w-40'>
-                  <Input placeholder={name} {...field} />
-                </FormControl>
-                <FormDescription>
-                  {description}
-                </FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-      )
-
-      return (
-        <div className='flex justify-center items-center min-h-screen bg-gray-900 text-white'>
-            <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-              <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                  {renderField("FullName", "Full Name", "Enter your full name.")}
-                  {renderField("Email", "Email", "Enter your Email address.")}
-                  {renderField("ContactNumber", "Contact Number", "Enter your contact number.")}
-                  {renderField("Education", "Education", "Enter your Educational background.")}
-                  {renderField("CareerGoals", "Career Goals", "Enter your career goals.")}
-                  {renderField("Interests", "Interests", "Enter your Interests.")}
-                  {renderField("Skills", "Skills", "Enter your Skills.")}
-                  {renderField("WorkExperience", "Work Experience", "Enter your work experience.")}
-                  {renderField("LearningMethods", "Learning Methods", "Enter your preferred learning methods.")}
-                  <div className="flex-span-full justify-center">
-                    <Button type="submit">Submit</Button>
-                  </div>
-                </form>
-              </Form>
-            </div>
+        <div className="w-full md:w-1/2 px-2 mb-4">
+          <FormField
+              control={form.control}
+              name={name}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>{label}</FormLabel>
+                  <FormControl>
+                    <Input placeholder={name} {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    {description}
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
         </div>
       )
-}
+      
+      return (
+        <div className='mt-10 flex justify-center items-center min-h-screen text-white'>
+          <div className="w-full md:max-w-4xl p-8 mx-auto bg-gray-900 rounded-xl shadow-md space-y-4 text-gray-900">
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                <div className="flex flex-wrap">
+                  {renderField("FullName", "Full Name", "Enter your full name.")}
+                  {renderField("Email", "Email", "Enter your Email address.")}
+                </div>
+                <div className="flex flex-wrap">
+                  {renderField("CareerGoals", "Career Goals", "Enter your career goals.")}
+                  {renderField("Education", "Education", "Enter your Educational background.")}
+                </div>
+                {renderField("Interests", "Interests", "Enter your Interests.")}
+                {renderField("Skills", "Skills", "Enter your Skills.")}
+                {renderField("WorkExperience", "Work Experience", "Enter your work experience.")}
+                {renderField("LearningMethods", "Learning Methods", "Enter your preferred learning methods.")}
+                <div className="flex justify-center">
+                  <Button type="submit">Submit</Button>
+                </div>
+              </form>
+            </Form>
+          </div>
+        </div>
+      )
+}      
 
 export default ProfileHero
