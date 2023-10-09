7c5ccc4d79c734da69c4a580694a6d675acf9470
Kinshu-Learner <kinshurajgupta1@gmail.com>
2023-10-09 20:56:28 +0530

    Added basic responsiveness to navbar

--- frontend/components/ProfileEdit.tsx
+++ frontend/components/ProfileEdit.tsx
@@ -46,83 +46,84 @@ const formSchema = z.object({
 })
 
 const ProfileEdit = () => {
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-          FullName: "",
-          Email: "",
-          ContactNumber: "",
-          Education: "",
-          CareerGoals: "",
-          Interests: "",
-          Skills: "",
-          WorkExperience: "",
-          LearningMethods: "",
-        },
-      })
-      function onSubmit() {
-        console.log("form submitted")
-      }
-      type FormFieldName = "FullName" | "Email" | "Education" | "CareerGoals" | "Interests" | "Skills" | "WorkExperience" | "LearningMethods";
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      FullName: "",
+      Email: "",
+      ContactNumber: "",
+      Education: "",
+      CareerGoals: "",
+      Interests: "",
+      Skills: "",
+      WorkExperience: "",
+      LearningMethods: "",
+    },
+  })
+  function onSubmit() {
+    console.log("form submitted")
+  }
+  type FormFieldName = "FullName" | "Email" | "Education" | "CareerGoals" | "Interests" | "Skills" | "WorkExperience" | "LearningMethods";
 
-      const placeholders = {
-        FullName: "Please enter your full name here",
-        Email: "Please enter your email here",
-        ContactNumber: "Please enter your contact number here",
-        Education: "Please enter your educational background here",
-        CareerGoals: "Please enter your career goals here",
-        Interests: "Please enter your interests here",
-        Skills: "Please enter your skills here",
-        WorkExperience: "Please enter your work experience here",
-        LearningMethods: "Please enter your preferred learning methods here"
-      };
-      
-      const renderField = (name: FormFieldName, label: string, description: string) => (
-        <div className="w-full md:w-1/2 px-2 mb-4">
-          <FormField
-              control={form.control}
-              name={name}
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>{label}</FormLabel>
-                  <FormControl>
-                    <Input className='focus:bg-transparent' placeholder={placeholders[name]} {...field} />
-                  </FormControl>
-                  <FormDescription>
-                    {description}
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-        </div>
-      )
-            
-      return (
-        <div className='mt-10 flex justify-center items-center min-h-screen '>
-          <div className="w-full md:max-w-6xl p-8 mx-auto bg-gradient-to-tr from-blue-500 to-indigo-700 rounded-xl shadow-md space-y-4 text-white">
-            <Form {...form}>
-              <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                <div className="flex flex-wrap">
-                  {renderField("FullName", "Full Name", "Enter your full name.")}
-                  {renderField("Email", "Email", "Enter your Email address.")}
-                </div>
-                <div className="flex flex-wrap">
-                  {renderField("CareerGoals", "Career Goals", "Enter your career goals.")}
-                  {renderField("Education", "Education", "Enter your Educational background.")}
-                </div>
-                {renderField("Interests", "Interests", "Enter your Interests.")}
-                {renderField("Skills", "Skills", "Enter your Skills.")}
-                {renderField("WorkExperience", "Work Experience", "Enter your work experience.")}
-                {renderField("LearningMethods", "Learning Methods", "Enter your preferred learning methods.")}
-                <div className="flex justify-center">
-                  <Button type="submit">Submit</Button>
-                </div>
-              </form>
-            </Form>
-          </div>
-        </div>
-      )
-}      
+  const placeholders = {
+    FullName: "Please enter your full name here",
+    Email: "Please enter your email here",
+    ContactNumber: "Please enter your contact number here",
+    Education: "Please enter your educational background here",
+    CareerGoals: "Please enter your career goals here",
+    Interests: "Please enter your interests here",
+    Skills: "Please enter your skills here",
+    WorkExperience: "Please enter your work experience here",
+    LearningMethods: "Please enter your preferred learning methods here"
+  };
+
+  const renderField = (name: FormFieldName, label: string, description: string) => (
+    <div className="w-full md:w-1/2 px-2 mb-4">
+      <FormField
+        control={form.control}
+        name={name}
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>{label}</FormLabel>
+            <FormControl>
+              <Input className='focus:bg-transparent' placeholder={placeholders[name]} {...field} />
+            </FormControl>
+            <FormDescription>
+              {description}
+            </FormDescription>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+    </div>
+  )
+
+  return (
+    <div className='mt-10 flex justify-center items-center min-h-screen'>
+      <div className="w-full md:max-w-6xl p-8 mx-auto bg-gradient-to-tr from-blue-500 to-indigo-700 rounded-xl shadow-md space-y-4 text-white">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <div className="flex flex-wrap">
+              {renderField("FullName", "Full Name", "Enter your full name.")}
+              {renderField("Email", "Email", "Enter your Email address.")}
+            </div>
+            <div className="flex flex-wrap">
+              {renderField("CareerGoals", "Career Goals", "Enter your career goals.")}
+              {renderField("Education", "Education", "Enter your Educational background.")}
+            </div>
+            {renderField("Interests", "Interests", "Enter your Interests.")}
+            {renderField("Skills", "Skills", "Enter your Skills.")}
+            {renderField("WorkExperience", "Work Experience", "Enter your work experience.")}
+            {renderField("LearningMethods", "Learning Methods", "Enter your preferred learning methods.")}
+            <div className="flex justify-center">
+              <Button type="submit">Submit</Button>
+            </div>
+          </form>
+        </Form>
+      </div>
+    </div>
+
+  )
+}
 
 export default ProfileEdit
