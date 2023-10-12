0ce62c7f215c8ee3ac8884c501f5d5888e06262e
Deepesh Kumar <deepesh.kumar.ug21@nsut.ac.in>
2023-10-13 01:00:58 +0530

    Profile hero idea

--- frontend/components/ProfileHero.tsx
+++ frontend/components/ProfileHero.tsx
@@ -1,20 +1,119 @@
-import React from 'react'
-import { FiEdit } from "react-icons/fi";
+import React from 'react';
+import { useState } from 'react';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { useForm } from "react-hook-form";
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import * as z from "zod"
+
+const formSchema = z.object({
+  FullName: z.string().min(2, {
+    message: "Full name must be at least 2 characters.",
+  }),
+  Email: z.string().email({
+    message: "Please enter a valid Email address.",
+  }),
+  ContactNumber: z.string().min(10, {
+    message: "Contact number must be at least 10 digits.",
+  }),
+  Education: z.string().min(2, {
+    message: "Please enter your Educational background.",
+  }),
+  CareerGoals: z.string().min(2, {
+    message: "Please enter your career goals.",
+  }),
+  Interests: z.string().min(2, {
+    message: "Please enter your Interests.",
+  }),
+  Skills: z.string().min(2, {
+    message: "Please enter your Skills.",
+  }),
+  WorkExperience: z.string().min(2, {
+    message: "Please enter your work experience.",
+  }),
+})
 
 const ProfileHero = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      FullName: "",
+      Email: "",
+      ContactNumber: "",
+      Education: "",
+      CareerGoals: "",
+      Interests: "",
+      Skills: "",
+      WorkExperience: "",
+    },
+  })
+  function onSubmit() {
+    console.log("form submitted")
+  }
+  type FormFieldName = "FullName" | "Email" | "Education" | "CareerGoals" | "Interests" | "Skills" | "WorkExperience";
+
+  const Values = {
+    FullName: "Deepesh Kumar",
+    Email: "Deepesh@example.com",
+    Education: "NULL buddy NULL",
+    CareerGoals: "You Kidding?",
+    Interests: "Coding",
+    Skills: "I don't have any",
+    WorkExperience: "Null",
+  };
+  const [isEditable, setIsEditable] = useState(false);
+  const renderField = (name: FormFieldName, label: string, description: string) => (
+    <div className="w-full md:w-1/2 px-2 mb-4">
+      <FormField
+        control={form.control}
+        name={name}
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>{label}</FormLabel>
+            <FormControl>
+              <Input placeholder={Values[name]} {...field} readOnly={!isEditable} />
+            </FormControl>
+            <FormDescription>
+              {description}
+            </FormDescription>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+    </div>
+  )
+
   return (
-    <div className='mt-10 flex min-h-screen'>
-      <div className="w-full md:max-w-6xl p-8 mx-auto bg-secondary rounded-xl shadow-md space-y-4 text-primary relative">
-        <div className="absolute top-0 right-0 p-2"><FiEdit size={25}/></div>
-        <h2>Full Name: Deepesh Kumar</h2>
-        <h2>Email: deepesh@example.com</h2>
-        <h2>Career Goals: Software Developer</h2>
-        <h2>Education Background: Bachelor's in Computer Science</h2>
-        <h2>Interests: Coding, Reading, Travelling</h2>
-        <h2>Skills: React, Node.js, Python</h2>
-        <h2>Work Experience: 3 years at XYZ Corp</h2>
+    <div className='mt-10 flex justify-center items-center min-h-screen'>
+      <div className="w-full md:max-w-6xl p-8 mx-auto bg-secondary rounded-xl shadow-md space-y-4 text-primary">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <div className="flex flex-wrap">
+              {renderField("FullName", "Full Name", "Enter your full name.")}
+              {renderField("Email", "Email", "Enter your Email address.")}
+            </div>
+            <div className="flex flex-wrap">
+              {renderField("CareerGoals", "Career Goals", "Enter your career goals.")}
+              {renderField("Education", "Education", "Enter your Educational background.")}
+            </div>
+            {renderField("Interests", "Interests", "Enter your Interests.")}
+            {renderField("Skills", "Skills", "Enter your Skills.")}
+            {renderField("WorkExperience", "Work Experience", "Enter your work experience.")}
+          </form>
+        </Form>
       </div>
     </div>
+
   )
 }
 
--- frontend/pages/profile/[[...index]].tsx
+++ frontend/pages/profile/[[...index]].tsx
@@ -12,4 +12,3 @@ const CompletedProfile = () => {
   )
 }
 export default CompletedProfile
-
