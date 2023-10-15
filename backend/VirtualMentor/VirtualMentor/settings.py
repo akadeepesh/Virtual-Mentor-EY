4d805ee30dfe41bda71346537c0e36e8bae6a701
Deepesh Kumar <deepesh.kumar.ug21@nsut.ac.in>
2023-10-15 15:12:20 +0530

    Added AWS RDS postgresql server

--- backend/VirtualMentor/VirtualMentor/settings.py
+++ backend/VirtualMentor/VirtualMentor/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'VirtualMentor.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'akadeepesh',
+        'PASSWORD': 'deepesh1710',
+        'HOST': 'virtualmentor.ceihqzhqo0wz.ap-northeast-1.rds.amazonaws.com',
+        'PORT': '5432'
     }
 }
 
