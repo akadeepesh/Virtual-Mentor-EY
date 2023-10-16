8c2b88754f4549eb2def4ba02f8ef377fc698ca8
Deepesh Kumar <deepesh.kumar.ug21@nsut.ac.in>
2023-10-16 14:00:02 +0530

    migrations changed, removed slug

--- backend/VirtualMentor/userData/models.py
+++ backend/VirtualMentor/userData/models.py
@@ -1,19 +1,11 @@
 from django.db import models
 
 class Students(models.Model):
-    full_name = models.CharField(max_length=100)
-    username = models.CharField(max_length=100, unique=True, blank=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    username = models.CharField(max_length=100, unique=True)
     email = models.EmailField()
     languages = models.TextField()
     skills = models.TextField()
     state = models.CharField(max_length=100)
     interests = models.TextField()
-    def save(self, *args, **kwargs):
-        if not self.username:
-            self.username = (self.full_name).split()[1]
-            if Students.objects.filter(username=self.username).exists():
-                i = 1
-                while Students.objects.filter(username=f"{self.username}{i}").exists():
-                    i += 1
-                self.username = f"{self.username}{i}"
-        super().save(*args, **kwargs)
