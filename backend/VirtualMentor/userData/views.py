4a192b938f38a42a91d24406a8645594fdc02228
Deepesh Kumar <deepesh.kumar.ug21@nsut.ac.in>
2023-10-19 04:14:09 +0530

    just touch up

--- backend/VirtualMentor/userData/views.py
+++ backend/VirtualMentor/userData/views.py
@@ -1,11 +1,10 @@
-from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import Students
 from .serializers import StudentSerializer
 
 @api_view(['GET'])
-def getStudent(request):
+def getStudent():
     queryset = Students.objects.all()
     serializer = StudentSerializer(queryset, many=True)
     return Response(serializer.data)
