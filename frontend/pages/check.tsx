0c192db52f6e7ceba138e64219208616a83baea9
Deepesh Kumar <deepesh.kumar.ug21@nsut.ac.in>
2023-10-29 16:02:33 +0530

    prettier

--- frontend/pages/check.tsx
+++ frontend/pages/check.tsx
@@ -1,6 +1,6 @@
 import { Input } from "@/components/ui/input";
 import { useUser } from "@clerk/nextjs";
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import { Label } from "@/components/ui/label";
 import {
   Card,
@@ -27,14 +27,14 @@ type FormData = {
 const Form = () => {
   const { user } = useUser();
   const [formData, setFormData] = useState<FormData>({
-    firstName: '',
-    lastName: '',
-    email: '',
-    username: '',
-    skills: '',
-    languages: '',
-    interests: '',
-    gender: '',
+    firstName: "",
+    lastName: "",
+    email: "",
+    username: "",
+    skills: "",
+    languages: "",
+    interests: "",
+    gender: "",
   });
   const [section, setSection] = useState<number>(1);
   const [errors, setErrors] = useState<Partial<FormData>>({});
@@ -43,9 +43,9 @@ const Form = () => {
     if (user) {
       setFormData((prevData) => ({
         ...prevData,
-        firstName: user.firstName || '',
-        lastName: user.lastName || '',
-        email: user.emailAddresses[0].emailAddress || '',
+        firstName: user.firstName || "",
+        lastName: user.lastName || "",
+        email: user.emailAddresses[0].emailAddress || "",
       }));
     }
   }, [user]);
@@ -65,12 +65,16 @@ const Form = () => {
       }
       if (!formData.email.trim()) {
         formErrors.email = "Email can't be empty";
-      } else if (!/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(formData.email.trim())) {
+      } else if (
+        !/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(
+          formData.email.trim()
+        )
+      ) {
         formErrors.email = "Email is not valid";
       }
-      if (!formData.username.trim()){
+      if (!formData.username.trim()) {
         formErrors.username = "Username can't be empty";
-      } else if (formData.username.trim().length < 3) { 
+      } else if (formData.username.trim().length < 3) {
         formErrors.username = "Username should be more than 3 letters";
         // If username already present in database then don't accept.
       }
@@ -97,24 +101,27 @@ const Form = () => {
 
   useEffect(() => {
     const handleKeyPress = (event: KeyboardEvent) => {
-      if (event.key === 'Enter') {
+      if (event.key === "Enter") {
         if (section === 1) {
           handleNextClick();
         } else if (section === 2) {
-          const form = document.querySelector('form');
+          const form = document.querySelector("form");
           if (form) {
-            const formEvent = new Event('submit', { bubbles: true, cancelable: true });
+            const formEvent = new Event("submit", {
+              bubbles: true,
+              cancelable: true,
+            });
             form.dispatchEvent(formEvent);
           }
         }
-      } else if (event.key === 'Escape' && section === 2) {
+      } else if (event.key === "Escape" && section === 2) {
         handlePreviousClick();
       }
     };
 
-    document.addEventListener('keydown', handleKeyPress);
+    document.addEventListener("keydown", handleKeyPress);
     return () => {
-      document.removeEventListener('keydown', handleKeyPress);
+      document.removeEventListener("keydown", handleKeyPress);
     };
   }, [section]);
 
@@ -135,51 +142,121 @@ const Form = () => {
                 <div className="flex flex-wrap mb-4">
                   <div className="w-full md:w-1/2 md:pr-2">
                     <Label>First Name</Label>
-                    <Input type="text" name="firstName" value={formData.firstName} onChange={(e) => setFormData({ ...formData, firstName: e.target.value })} />
-                    {errors.firstName && <span className="text-red-400 text-sm">{errors.firstName}</span>}
+                    <Input
+                      type="text"
+                      name="firstName"
+                      value={formData.firstName}
+                      onChange={(e) =>
+                        setFormData({ ...formData, firstName: e.target.value })
+                      }
+                    />
+                    {errors.firstName && (
+                      <span className="text-red-400 text-sm">
+                        {errors.firstName}
+                      </span>
+                    )}
                   </div>
                   <div className="w-full md:w-1/2 md:pl-2">
                     <Label>Last Name</Label>
-                    <Input type="text" name="lastName" value={formData.lastName} onChange={(e) => setFormData({ ...formData, lastName: e.target.value })} />
-                    {errors.lastName && <span className="text-red-400 text-sm">{errors.lastName}</span>}
+                    <Input
+                      type="text"
+                      name="lastName"
+                      value={formData.lastName}
+                      onChange={(e) =>
+                        setFormData({ ...formData, lastName: e.target.value })
+                      }
+                    />
+                    {errors.lastName && (
+                      <span className="text-red-400 text-sm">
+                        {errors.lastName}
+                      </span>
+                    )}
                   </div>
                 </div>
                 <div className="mb-4">
                   <Label>Email</Label>
-                  <Input type="text" name="email" value={formData.email} onChange={(e) => setFormData({ ...formData, email: e.target.value })} />
-                  {errors.email && <span className="text-red-400 text-sm">{errors.email}</span>}
+                  <Input
+                    type="text"
+                    name="email"
+                    value={formData.email}
+                    onChange={(e) =>
+                      setFormData({ ...formData, email: e.target.value })
+                    }
+                  />
+                  {errors.email && (
+                    <span className="text-red-400 text-sm">{errors.email}</span>
+                  )}
                 </div>
                 <div className="mb-4">
                   <Label>Username</Label>
-                  <Input type="text" name="username" value={formData.username} onChange={(e) => setFormData({...formData, username: e.target.value })} />
-                  {errors.username && <span className="text-red-400 text-sm">{errors.username}</span>}
+                  <Input
+                    type="text"
+                    name="username"
+                    value={formData.username}
+                    onChange={(e) =>
+                      setFormData({ ...formData, username: e.target.value })
+                    }
+                  />
+                  {errors.username && (
+                    <span className="text-red-400 text-sm">
+                      {errors.username}
+                    </span>
+                  )}
                 </div>
                 <div className="justify-between">
-                  <Button variant={"ghost"} onClick={handleNextClick}>Next <ArrowRight className="w-4 h-4 ml-2" /></Button>
+                  <Button variant={"ghost"} onClick={handleNextClick}>
+                    Next <ArrowRight className="w-4 h-4 ml-2" />
+                  </Button>
                 </div>
               </div>
             )}
 
             {section === 2 && (
-              <div className='flex flex-col'>
+              <div className="flex flex-col">
                 <div className="mb-4">
                   <Label>Skills</Label>
-                  <Input type="text" name="skills" onChange={(e) => setFormData({ ...formData, skills: e.target.value })} />
+                  <Input
+                    type="text"
+                    name="skills"
+                    onChange={(e) =>
+                      setFormData({ ...formData, skills: e.target.value })
+                    }
+                  />
                 </div>
                 <div className="mb-4">
                   <Label>Languages</Label>
-                  <Input type="text" name="languages" onChange={(e) => setFormData({ ...formData, languages: e.target.value })} />
+                  <Input
+                    type="text"
+                    name="languages"
+                    onChange={(e) =>
+                      setFormData({ ...formData, languages: e.target.value })
+                    }
+                  />
                 </div>
                 <div className="mb-4">
                   <Label>Interests</Label>
-                  <Input type="text" name="interests" onChange={(e) => setFormData({...formData, interests: e.target.value })} />
+                  <Input
+                    type="text"
+                    name="interests"
+                    onChange={(e) =>
+                      setFormData({ ...formData, interests: e.target.value })
+                    }
+                  />
                 </div>
                 <div className="mb-4">
                   <Label>Gender</Label>
-                  <Input type="text" name="gender" onChange={(e) => setFormData({ ...formData, gender: e.target.value })} />
+                  <Input
+                    type="text"
+                    name="gender"
+                    onChange={(e) =>
+                      setFormData({ ...formData, gender: e.target.value })
+                    }
+                  />
                 </div>
                 <div className="flex justify-between">
-                  <Button variant={"ghost"} onClick={handlePreviousClick}>Previous</Button>
+                  <Button variant={"ghost"} onClick={handlePreviousClick}>
+                    Previous
+                  </Button>
                   <Button type="submit">Submit</Button>
                 </div>
               </div>
