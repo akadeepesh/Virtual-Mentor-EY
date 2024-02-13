92443f599251715b39ee2d2f4d2167720408d978
Deepesh Kumar <deepesh.kumar.ug21@nsut.ac.in>
2024-02-13 15:47:22 +0530

    Added Home page template

---
+++ frontend/components/Home.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Button } from "./ui/button";
+import { useUser } from "@clerk/nextjs";
+
+const HomePage: React.FC = () => {
+  const { user } = useUser();
+  return (
+    <div className="flex items-center justify-center mt-10">
+      <div className="flex flex-col  max-w-screen-lg">
+        <h1 className="text-4xl font-bold mb-4">
+          Welcome {user?.firstName}, to VirtualMentor
+        </h1>
+        <div className="">
+          <p className="text-lg text-gray-600">
+            This is the home page and still building, Thanks for your paitence.
+          </p>
+          <Button className="px-4 py-2 mt-4">Get Started</Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,16 +1,21 @@
 import HomeNav from "@/components/HomeNav";
-import Head from "next/head"
+import Home from "@/components/Home";
+import Head from "next/head";
+
 const HomePage = () => {
   return (
-    <div className="flex justify-center">
-      <Head>
-        <title>Home</title>
-      </Head>
-      <div className="flex justify-center items-center border-b border-gray-600 mx-2">
-        <HomeNav />
+    <div>
+      <div className="flex justify-center">
+        <Head>
+          <title>Home</title>
+        </Head>
+        <div className="flex justify-center items-center border-b border-gray-600 mx-2">
+          <HomeNav />
+        </div>
       </div>
+      <Home />
     </div>
   );
-}
+};
 
 export default HomePage;
