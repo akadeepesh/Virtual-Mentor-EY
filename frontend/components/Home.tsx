953e1d239695d500e3858218582580e275bf6dfc
Deepesh Kumar <deepesh.kumar.ug21@nsut.ac.in>
2024-02-15 02:29:12 +0530

    Add progress bar and profile completion button to home page

--- frontend/components/Home.tsx
+++ frontend/components/Home.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-// import { Button } from "./ui/button";
+import { Button } from "./ui/button";
 // import { useUser } from "@clerk/nextjs";
 // import Link from "next/link";
 import { SparklesCore } from "./ui/sparkles";
 import { TypewriterEffectSmooth } from "./ui/typewriter-effect";
-
+import { Progress } from "@/components/ui/progress";
+import { ChevronLeftIcon, ChevronRightIcon } from "@radix-ui/react-icons";
 const HomePage: React.FC = () => {
   // const { user } = useUser();
   const wordsOne = [
@@ -25,21 +26,6 @@ const HomePage: React.FC = () => {
     },
   ];
   return (
-    // <div className="flex items-center justify-center mt-10">
-    //   <div className="flex flex-col  max-w-screen-lg">
-    //     <h1 className="text-4xl font-bold mb-4">
-    //       Welcome {user?.firstName}, to VirtualMentor
-    //     </h1>
-    //     <div className="">
-    //       <p className="text-lg text-gray-600">
-    //         This is the home page and still building, Thanks for your paitence.
-    //       </p>
-    //       <Link href={"/profile"}>
-    //         <Button className="px-4 py-2 mt-4">Get Started</Button>
-    //       </Link>
-    //     </div>
-    //   </div>
-    // </div>
     <div className="h-[46rem] md:h-[40rem] relative md:w-full flex flex-col items-center justify-center overflow-hidden rounded-md">
       <SparklesCore
         id="tsparticlesfullpage"
@@ -52,11 +38,17 @@ const HomePage: React.FC = () => {
       />
       <div className="md:text-7xl text-3xl lg:text-6xl font-bold text-center text-primary relative z-20 select-none">
         <TypewriterEffectSmooth cursorClassName="bg-primary" words={wordsOne} />
-        <div className="bg-gradient-to-tr from-blue-500 to-indigo-700 bg-clip-text text-transparent">
+        <div className="bg-gradient-to-tr from-blue-500 to-indigo-700 text-5xl lg:text-6xl bg-clip-text text-transparent">
           VirtualMentor
         </div>
         <br />
-        <div className="text-base">View Your Profile</div>
+        <div className="border rounded-xl flex flex-col items-start">
+          <Progress className="h-1 ml-4 mt-2 w-[90%]" value={33} />
+          <Button variant={"link"} className="text-base flex flex-row">
+            <div className="">Complete Your Profile</div>
+            <ChevronRightIcon className="mt-1" />
+          </Button>
+        </div>
       </div>
     </div>
   );
