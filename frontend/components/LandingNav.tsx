311a62af98db11d3e980b3af99a55878b9c55a0a
Deepesh Kumar <deepesh.kumar.ug21@nsut.ac.in>
2024-03-06 14:41:48 +0530

    Fixed landing navbar

--- frontend/components/LandingNav.tsx
+++ frontend/components/LandingNav.tsx
@@ -1,29 +1,31 @@
-import Link from "next/link";
 import { ThemeToggle } from "./ThemeToggle";
+import { SignInButton, SignUpButton } from "@clerk/nextjs";
+import { useRouter } from "next/router";
 
 const LandingNav = () => {
+  const router = useRouter();
   return (
     <nav className="flex md:mt-4 justify-between h-20 items-center mx-auto max-w-screen-xl w-screen">
-      <a
-        href="/"
-        className="font-bold text-xl md:text-2xl mx-1 md:mx-4 duration-200"
+      <div
+        onClick={() => router.push("/")}
+        className="font-bold text-xl md:text-2xl mx-1 md:mx-4 duration-200 cursor-pointer"
       >
         VirtualMentor
-      </a>
+      </div>
       <div className="flex justify-end items-center">
-        <span className="">
-          <Link
-            href={"/sign-up"}
+        <span className="flex">
+          <div
+            onClick={() => router.push("/sign-up")}
             className="bg-white text-black rounded-md py-2 md:mx-2 px-3 md:px-5 cursor-pointer font-medium hover:bg-gray-300 duration-200"
           >
-            Sign up
-          </Link>
-          <Link
-            href={"/sign-in"}
+            <SignUpButton />
+          </div>
+          <div
+            onClick={() => router.push("/sign-in")}
             className="rounded-md bg-blue-700 py-2 mx-2 px-3 md:px-5 cursor-pointer font-medium hover:bg-blue-800 duration-200"
           >
-            Login
-          </Link>
+            <SignInButton />
+          </div>
         </span>
         <ThemeToggle className="" />
       </div>
